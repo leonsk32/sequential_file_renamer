0592689154c9fcc8282f0f1e36ff7ce0b31f47f9
leon <leonsk32@gmail.com>
2020-09-11 20:44:15 +0900

    add confirmation

--- file.sh
+++ file.sh
@@ -1,17 +1,28 @@
-echo $1
-i=1
+echo "-------------------------------------------"
+echo "target directory: $1"
+echo "-------------------------------------------"
 
-while true;
-do
+i=1
+echo "-------------------------------------------"
+echo "existing files:"
+ls "$1"
+echo "-------------------------------------------"
+echo 'These existing files may be renamed. Is it ok? [y/N]'
+read -r
+
+if [[ $REPLY != 'y' ]]; then
+  exit 0
+fi
+
+while true; do
   array=$(ls "$1")
   for file in ${array[@]}; do
-    if ! [[ $file =~ ^[0-9]{3}\..+$ ]] ;
-    then
+    if ! [[ $file =~ ^[0-9]{3}\..+$ ]]; then
       seq=$(printf "%03d" $i)
       ext=${file##*.}
       mv "$1/$file" "$1/$seq.$ext"
       echo "$1/$seq.$ext"
-      i=$(( $i + 1 ))
+      i=$((i + 1))
     fi
   done
   sleep 1
