71d0fb6b9dd82a89259e167f6ca622897d31717e
leon <leonsk32@gmail.com>
2020-09-11 20:54:32 +0900

    set starting sequence by arguments

--- file.sh
+++ file.sh
@@ -1,13 +1,22 @@
+declare seq
+if [[ $# == 1 ]]; then
+  seq=1
+elif [[ $# == 2 ]]; then
+  seq=$2
+fi
+
 echo "-------------------------------------------"
 echo "target directory: $1"
 echo "-------------------------------------------"
+echo "-------------------------------------------"
+echo "sequence will start from $seq"
+echo "-------------------------------------------"
 
-i=1
 echo "-------------------------------------------"
 echo "existing files:"
 ls "$1"
 echo "-------------------------------------------"
-echo 'These existing files may be renamed. Is it ok? [y/N]'
+echo '!!!!! These existing files may be renamed !!!!! continue? [y/N]'
 read -r
 
 if [[ $REPLY != 'y' ]]; then
@@ -15,14 +24,14 @@ if [[ $REPLY != 'y' ]]; then
 fi
 
 while true; do
-  array=$(ls "$1")
-  for file in ${array[@]}; do
+  files=$(ls "$1")
+  for file in ${files[@]}; do
     if ! [[ $file =~ ^[0-9]{3}\..+$ ]]; then
-      seq=$(printf "%03d" $i)
-      ext=${file##*.}
-      mv "$1/$file" "$1/$seq.$ext"
-      echo "$1/$seq.$ext"
-      i=$((i + 1))
+      sequence=$(printf "%03d" $seq)
+      extension=${file##*.}
+      mv "$1/$file" "$1/$sequence.$extension"
+      echo "$1/$file was renamed to $1/$sequence.$extension"
+      seq=$((seq + 1))
     fi
   done
   sleep 1
